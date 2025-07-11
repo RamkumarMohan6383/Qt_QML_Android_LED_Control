ad28dae6f1fc9bc292532a5ded282cfbcee921c2
RamkumarMohan6383 <95962773+RamkumarMohan6383@users.noreply.github.com>
2025-07-11 10:53:28 +0530

    V1.01

---
+++ android/src/org/myapp/gpio/GpioJNI.java
@@ -0,0 +1,10 @@
+package org.myapp.gpio;
+
+public class GpioJNI {
+    static {
+        System.loadLibrary("gpiojni");
+    }
+
+    public static native void gpioWrite(int pin, int value);  // ✅ static
+    public static native void gpioPWM(int pin, int duty);     // ✅ static
+}

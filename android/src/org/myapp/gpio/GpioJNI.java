f1b8018ac0b7a8f54debc0ab885c0b57b5199e4d
RamkumarMohan6383 <95962773+RamkumarMohan6383@users.noreply.github.com>
2025-07-11 13:21:31 +0530

    V1.01 With Software PWM Control

--- android/src/org/myapp/gpio/GpioJNI.java
+++ android/src/org/myapp/gpio/GpioJNI.java
@@ -1,10 +1,9 @@
 package org.myapp.gpio;
-
 public class GpioJNI {
     static {
         System.loadLibrary("gpiojni");
     }
-
     public static native void gpioWrite(int pin, int value);  // ✅ static
     public static native void gpioPWM(int pin, int duty);     // ✅ static
+    public static native void setGpioPermission();
 }
